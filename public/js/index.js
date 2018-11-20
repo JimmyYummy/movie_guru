6362803b98c929680dad9c2cdc8473aef8e4166d
crestwood204 <andong@sas.upenn.edu>
2018-11-20 16:47:02 -0500

    add css and js directories

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+
+  $('#button').on('click', function() {
+    console.log('clicked');
+    $.ajax({
+      url: '/api_endpoint/123/?query_message=hello',
+      method: 'post',
+      data: {
+        message: $('#movie').text().trim()
+      },
+      error(err) {
+        console.log('Error:', err);
+      },
+      success(res) {
+        console.log('success');
+      }
+    })
+  });
+});
